45bb6d29298a081e3e3b92a54acce3a569a9726e
admin <ariel.rubi@gmail.com>
2020-08-06 21:00:16 +0300

    .

--- ConsoleClient/WinformClient/Program.cs
+++ ConsoleClient/WinformClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,15 +15,29 @@ namespace WinformClient
         [STAThread]
         static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
             bool devMode = false;
             if (args != null && args.Length > 0 && args[0] == "dev")
             {
                 devMode = true;
             }
 
+
+            if (devMode == false)
+            {
+                string proc = Process.GetCurrentProcess().ProcessName;
+                // get the list of all processes by that name
+                Process[] processes = Process.GetProcessesByName(proc);
+                // if there is more than one process... => 1 is ME (da....)
+                if (processes.Length > 1)
+                {
+                    MessageBox.Show("only one instance at a time");
+                    return;
+                }
+            }
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ClientForm(devMode));
         }
     }
