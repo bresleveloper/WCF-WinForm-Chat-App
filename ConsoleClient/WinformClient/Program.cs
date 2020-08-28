74a6b3c953b7fc0d06db5f36b11c9c186cd9c2ec
admin <ariel.rubi@gmail.com>
2020-08-28 15:46:32 +0300

    .

--- ConsoleClient/WinformClient/Program.cs
+++ ConsoleClient/WinformClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -22,23 +23,59 @@ namespace WinformClient
             }
 
 
-            /*if (devMode == false)
+            if (devMode == false)
             {
                 string proc = Process.GetCurrentProcess().ProcessName;
                 // get the list of all processes by that name
                 Process[] processes = Process.GetProcessesByName(proc);
+
                 // if there is more than one process... => 1 is ME (da....)
                 if (processes.Length > 1)
                 {
-                    MessageBox.Show("only one instance at a time");
-                    return;
+                    IntPtr AnswerBytes;
+                    IntPtr AnswerCount;
+
+                    foreach (Process p in processes)
+                    {
+                        if (WTSQuerySessionInformationW(WTS_CURRENT_SERVER_HANDLE,
+                                                       p.SessionId,
+                                                       WTS_UserName,
+                                                       out AnswerBytes,
+                                                       out AnswerCount))
+                        {
+                            string userName = Marshal.PtrToStringUni(AnswerBytes);
+                            MessageBox.Show(userName + " - " + Environment.UserName + " - Only 1 instance per user allowed");
+                            return;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Could not access Terminal Services Server.");
+                        }
+                    }
+
                 }
-            }*/
+            }
 
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ClientForm(devMode));
         }
+
+
+
+        //just use the current TS server context.
+        internal static IntPtr WTS_CURRENT_SERVER_HANDLE = IntPtr.Zero;
+
+        //the User Name is the info we want returned by the query.
+        internal static int WTS_UserName = 5;
+
+        [DllImport("Wtsapi32.dll")]
+        public static extern bool WTSQuerySessionInformationW(
+            IntPtr hServer,
+            int SessionId,
+            int WTSInfoClass,
+            out IntPtr ppBuffer,
+            out IntPtr pBytesReturned);
     }
 }
